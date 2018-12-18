f05bcea6863f74f5f08f3129d981a1f5b8acba9e
osheng <osheng@users.noreply.github.com>
2018-12-18 13:28:13 -0500

    Update grading tool.py

--- grading tool.py
+++ grading tool.py
@@ -1,9 +1,3 @@
-"""I teach a Chinese class and I wanted a quick way to calculate my students'
-grades and to be able to weight their higher-mark work more favourably. Doing
-this in Sheets looked complicated so I came up with this program which takes in
-a csv file (with some formatting constraints) and prints out the students'
-grades and warns me of any work that they may have not handed in."""
-
 # TODO: Create a function to write to a CSV so that you can save the results
 # TODO: figure out how to encode Chinese characters
 
