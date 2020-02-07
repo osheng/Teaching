49d21af35841a445462891d10165896ebb4df825
osheng <osheng@gmail.com>
2020-02-06 23:30:06 -0500

    cleaned code

--- split_csv.py
+++ split_csv.py
@@ -1,19 +1,16 @@
+# Split one csv file into multiple
 import pandas as pd
 import sys, os
 from sys import argv
-print(len(argv))
+
 if len(argv) < 3:
     print("Usage: python3 split_csv.py path/ CSV_NAME.csv")
     sys.exit()
 
-if argv[2][-4:] != ".csv":
-    print("Usage: python3 split_csv.py path/ CSV_NAME.csv")
-    print(2)
-    sys.exit()
-
-argv2 = argv[1] if argv[1][-1] == "/" else argv[2] + "/"
-
 for arg in argv[2:]:
+    if arg[-4:] != ".csv":
+        print("Usage: python3 split_csv.py path/ CSV_NAME.csv")
+        sys.exit()
     curr_df = pd.read_csv(arg)
     sections_gb = curr_df.groupby('Section')
     sections_list = [sections_gb.get_group(x) for x in sections_gb.groups]
